b60a0325bee9ff861db18ef9c1e96198c88f0f66
sanjayrushee <rashmikafan05.com>
2024-08-17 00:45:32 +0530

    login done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {Route, Switch} from 'react-router-dom'
 import Login from './Components/Login';
+import Singup from './Components/Singup';
 import './App.css';
 
 const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
+    <Route exact path="/singup" component={Singup} />
+
   </Switch>
 )
 
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,63 +1,118 @@
 import { Component } from "react";
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+
+const Api_link_login = process.env.REACT_APP_API_URL_LOGIN
 
 class Login extends Component{
 
 
   state = {
-    name: '',
     email: '',
-    pasword: '',
+    password: '',
   }
 
-  onSubmitFunction = (event) => {
+  onSubmitFunction = async (event) => {
     event.preventDefault()
 
-    const {pasword,name,email} = this.setState
-    console.log(pasword,name,email)
+    const { email, password } = this.state;
+    console.log(password, email);
 
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Telling the server you're sending JSON
+      },
+      body: JSON.stringify({ 
+        email,
+        password
+      }),
+    };
+
+    try {
+      const response = await fetch(Api_link_login, options);
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(data.jwtToken);
+      } else {
+        console.log(data.error_msg);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 
-  handleChangename = (event) => {
-    this.setState({name: event.target.value})
-  }
 
   handleChangeemail = (event) => {
     this.setState({email: event.target.value})
   }
 
   handleChangepassword = (event) => {
-    this.setState({pasword: event.target.value})
+    this.setState({password: event.target.value})
   }
     
     render(){
-      const {name,email,pasword} = this.state
-        return(
-            <div className="flex items-center h-screen w-full bg-slate-900">
-            <div className="w-full bg-indigo-400 rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-            <span className="block w-full text-xl uppercase font-bold mb-4">Login</span>      
-              <form className="mb-4" onSubmit={this.onSubmitFunction}>
-                <div className="mb-4 md:w-full">
-                  <label for="email" className="block text-xs mb-1">Username</label>
-                  <input 
-                  type="text"
-                  name="name" onChange={this.handleChangename} value={name} className="w-full border rounded p-2  outline-none focus:shadow-outline"  id="email" placeholder="Username"/>
+      const {email,password} = this.state
+             return(
+              <div className="flex items-center justify-center h-screen w-full px-5 sm:px-0">
+                <div className="flex bg-white rounded-lg shadow-lg border overflow-hidden max-w-sm lg:max-w-4xl w-full">
+                  <div
+                    className="hidden md:block lg:w-3/4  bg-blue-700 backgroundimageforlogin"
+                  ></div>
+                  <form onSubmit={this.onSubmitFunction} className="w-full p-8 lg:w-1/2">
+                    <p className="text-xl text-gray-600 text-center">Welcome back!</p>
+                    <div className="mt-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2">
+                        Email Address
+                      </label>
+                  
+                      <input
+                        className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+                        type="email"
+                        required
+                        value={email}
+                        onChange={this.handleChangeemail}
+
+                      />
+                    </div>
+                    <div className="mt-4 flex flex-col justify-between">
+                      <div className="flex justify-between">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                          Password
+                        </label>
+                      </div>
+                      <input
+                        className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+                        type="password"
+                        required
+                        value={password}
+                        onChange={this.handleChangepassword}
+                      />
+                     
+                    </div>
+                    <div className="mt-8">
+                      <button  type="submit" className="bg-blue-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-600">
+                        Login
+                      </button>
+                    </div>
+                    <div className="mt-4 flex items-center w-full text-center">
+                      <p
+                        className="text-xs text-gray-500 capitalize text-center w-full"
+                      >
+                        Don&apos;t have any account yet?
+                        <Link to="/singup">
+                        <span className="text-blue-700"> Sign Up</span>
+                        </Link>
+                      </p>
+                    </div>
+                  </form>
                 </div>
-                <div className="mb-4 md:w-full">
-                  <label for="email" className="block text-xs mb-1">Email</label>
-                  <input 
-                  type="text"
-                  name="email"  value={email} onChange={this.handleChangeemail} className="w-full border rounded p-2 outline-none focus:shadow-outline"  id="email" placeholder="Email"/>
-                </div>
-                <div className="mb-6 md:w-full">
-                  <label for="password" className="block text-xs mb-1">Password</label>
-                  <input type="text"
-            name="pasword" value={pasword} onChange={this.handleChangepassword} className="w-full border rounded p-2 outline-none focus:shadow-outline"  id="password" placeholder="Password" />
-                </div>
-                <button type="button" className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded">Login</button>
-              </form>
-              <a className="text-blue-700 text-center text-sm" href="/login">Register Now</a>
-          </div>
-        </div>
+              </div>
+        
+          
         )
     }
 }
