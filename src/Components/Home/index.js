d0c50fd6408db9c39b185981fe0c5986b7211ab8
sanjayrushee <rashmikafan05.com>
2024-08-18 00:02:14 +0530

    Login full completed

---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h2>hello this is home</h2>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/Components/NotFound/index.js
@@ -0,0 +1,6 @@
+const NotFound = () =>(
+    <div>
+        <h1>notFound</h1>
+    </div>
+)
+export default NotFound
