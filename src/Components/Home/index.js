bb1b501c574f521453655f26077f34eee39cff12
sanjayrushee <rashmikafan05.com>
2024-08-20 15:50:29 +0530

    navbar added

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,15 @@
 import { Component } from "react";
+import Navbar from "../Navbar";
 
 class Home extends Component{
     render(){
         return(
+            <>
+            <Navbar />
             <div>
                 <h2>hello this is home</h2>
             </div>
+            </>
         )
     }
 }
