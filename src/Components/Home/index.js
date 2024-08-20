26c2321fc40b332503894dc4320752c7dd4c2dfb
sanjayrushee <rashmikafan05.com>
2024-08-20 15:58:32 +0530

    add something

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,6 +6,20 @@ class Home extends Component{
         return(
             <>
             <Navbar />
+            <aside className="fixed top-16 start-0 w-64 h-full bg-gray-50 dark:bg-gray-800 shadow-lg">
+        <div className="overflow-y-auto py-4 px-3">
+          <ul className="space-y-2">
+            <li>
+              <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v8m4-4H8" />
+                </svg>
+                <span className="ml-3">Notes</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
             <div>
                 <h2>hello this is home</h2>
             </div>
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,15 +1,19 @@
 const Navbar = () => (
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <div class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="" class="h-8" alt="Flowbite Logo"/>
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-  </div>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-  </div>
-  </div>
-</nav>
+    <>
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </div>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Log Out
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
   );
   
   export default Navbar;
